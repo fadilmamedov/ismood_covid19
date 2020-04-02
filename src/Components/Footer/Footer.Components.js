d971809d7ccd1e5384633d4e14e9a4c76558714f
Fadil Mamedov <fadil.mamedov@mail.ru>
2020-04-02 17:41:11 +0300

    Update footer layout and add Contact Us link

---
+++ src/Components/Footer/Footer.Components.js
@@ -0,0 +1,56 @@
+import styled from 'styled-components/macro';
+import { Navbar as NavbarBase } from 'react-bootstrap';
+
+const Navbar = styled(NavbarBase)`
+  color: #5c7080;
+  font-size: 14px;
+  box-shadow: 0 0 0 1px rgba(16, 22, 26, .1), 0 0 0 rgba(16, 22, 26, 0), 0 1px 1px rgba(16, 22, 26, .2);
+
+  @media screen and (max-width: 575px) {
+    flex-direction: column;
+    align-items: flex-start;
+  }
+`;
+
+const ExternalLink = styled.a.attrs({
+  target: '_blank',
+})`
+  margin: 0 4px;
+`;
+
+const RightContainer = styled.div`
+  display: flex;
+  margin-left: auto;
+
+  @media screen and (max-width: 575px) {
+    width: 100%;
+    margin-top: 20px;
+    margin-left: 0;
+    justify-content: space-between;
+  }
+`;
+
+const ContactUsLink = styled.a``;
+
+const DownloadImageButton = styled.button`
+  margin-left: 20px;
+  padding: 0;
+  border: 0;
+  color: #007bff;
+  background: transparent;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const InformationSourceDescription = styled.span``;
+
+export {
+  Navbar,
+  ExternalLink,
+  ContactUsLink,
+  RightContainer,
+  DownloadImageButton,
+  InformationSourceDescription,
+};
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,35 +3,19 @@ import * as r from 'ramda';
 import types from 'prop-types';
 import { connect } from 'react-redux';
 import html2canvas from 'html2canvas';
-import styled from 'styled-components/macro';
-import { Navbar as NavbarBase } from 'react-bootstrap';
 
 import { selectors } from 'Store';
 import { saveToFile } from 'Utilities';
 import * as translations from 'Assets/Translations';
 
-const Navbar = styled(NavbarBase)`
-  color: #5c7080;
-  font-size: 14px;
-  box-shadow: 0 0 0 1px rgba(16, 22, 26, .1), 0 0 0 rgba(16, 22, 26, 0), 0 1px 1px rgba(16, 22, 26, .2);
-`;
-
-const ExternalLink = styled.a.attrs({
-  target: '_blank',
-})`
-  margin: 0 4px;
-`;
-
-const DownloadImageButton = styled.button`
-  margin-left: auto;
-  padding: 0;
-  border: 0;
-  background: transparent;
-
-  &:hover {
-    text-decoration: underline;
-  }
-`;
+import {
+  Navbar,
+  ExternalLink,
+  ContactUsLink,
+  RightContainer,
+  DownloadImageButton,
+  InformationSourceDescription,
+} from './Footer.Components';
 
 const FooterBase = ({ language }) => {
   const Strings = translations[language];
@@ -64,16 +48,24 @@ const FooterBase = ({ language }) => {
   }, []);
 
   return (
-    <Navbar bg="light" fixed="bottom" className="footer">
-      {Strings.Footer.InformationSourceDescription.Prefix}
+    <Navbar bg="light" className="footer">
+      <InformationSourceDescription>
+        {Strings.Footer.InformationSourceDescription.Prefix}
 
-      <ExternalLink href="https://www.who.int/">WHO</ExternalLink>
-      {Strings.Footer.InformationSourceDescription.And}
-      <ExternalLink href="https://eody.gov.gr/en">NPHO</ExternalLink>
+        <ExternalLink href="https://www.who.int/">WHO</ExternalLink>
+        {Strings.Footer.InformationSourceDescription.And}
+        <ExternalLink href="https://eody.gov.gr/en">NPHO</ExternalLink>
+      </InformationSourceDescription>
 
-      <DownloadImageButton onClick={handleDownloadImageButtonClick}>
-        {Strings.Footer.DownloadImageButton}
-      </DownloadImageButton>
+      <RightContainer>
+        <ContactUsLink href="mailto: covid19@ismood.com">
+          {Strings.Footer.ContactUsLink}
+        </ContactUsLink>
+
+        <DownloadImageButton onClick={handleDownloadImageButtonClick}>
+          {Strings.Footer.DownloadImageButton}
+        </DownloadImageButton>
+      </RightContainer>
     </Navbar>
   )
 };
